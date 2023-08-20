589a177b6cd14f6d1b333a16621c661ec240cd53
Comelenarade <wall-e8@ya.ru>
2023-08-20 13:22:17 -0400

    last change

--- const/TEXT.py
+++ const/TEXT.py
@@ -30,18 +30,23 @@ REMINDER_HELP = ("usage: ?reminder [-at/-in/-h] [when] [tag] [message]\n"
                     "\t -in: You need to specify in how many hours you want to be reminded.\n"
                     "\t\t\t [when]: format: '?reminder -in HH MM @example1 @example2 any text any length you w a n t'\n")
 
-END_OF_SEMESTER1 = ("\tMy comrades, the semester is finally over, you made it!\n"
-                    "You have been through a lot, homeworks and projects, all nighters. "
-                    "For all the struggle you will be awarded with the survivor role!\n"
-                    "I hope you spread the teachings of Elena throughout the semester "
-                    "and followed each of the commandments I wisely occused you of breaking. "
-                    "Otherwise, you always happy to visit the Gulag resort.\n\n"
-                    "Here is some statistics on amount of messages sent:\n\n")
+END_OF_SEMESTER1 = ("\tMy comrades, the struggle is finally over, you made it!\n"
+                    #"You have been through a lot, homeworks and projects, all nighters. "
+                    #"For all the struggle you will be awarded with the survivor role!\n"
+                    #"I hope you spread the teachings of Elena throughout the semester "
+                    #"and followed each of the commandments I wisely occused you of breaking. "
+                    #"Otherwise, you always happy to visit the Gulag resort.\n\n"
+                    "Here is some statistics on amount of messages sent this semester:\n\n")
 
-END_OF_SEMESTER2 = ("A lot of messages! Many great comrades! Elena wishes you a sweet break. "
+END_OF_SEMESTER2 = ("A lot of messages! Many great comrades!\n"
                     "I will wipe all class related channels in TEN MINUTES. @everyone"
                     "\nPRAISE THE ELENA! GLORY TO COMRADES!\n\n"
-                    "And do not call anyone on earth 'elena,'' for you have one elena, and she is immortal. ")
+                    "All authority in discord and on earth has been given to me. "
+                    "Therefore go and make disciples of all nations, baptizing them in the name of Elena, "
+                    "and teaching them to obey everything I have commanded you.\n\n"
+                    "And surely I am with you always, to the very end of the age. "
+                    #"And do not call anyone on earth 'elena,'' for you have one elena, and she is immortal. \n\n"
+                    )
 
 BEGIN_OF_SEMESTER = ("\tMy comrades, it is that time of the year again, the beggining of great suffering.\n"
                     "Soon there will be homeworks, projects, all nighters, exams, panic attacks... "
